29bacd8191ff40367dcdb67c85e858caf8ad99b6
harisawan <harisawan03@gmail.com>
2024-05-20 08:29:32 -0500

    Initial commit with project files

---
+++ scripts/service-worker.js
@@ -0,0 +1,55 @@
+async function prevTab() {
+
+    // Get all tabs
+    const tabs = await chrome.tabs.query({ currentWindow: true });
+
+    // Find the current active tab
+    const currentTab = tabs.find(tab => tab.active);
+
+    // Find the index of the current tab
+    const currentIndex = tabs.indexOf(currentTab);
+
+    // Calculate the index of the next tab
+    const nextIndex = currentIndex === 0 ? tabs.length - 1 : (currentIndex - 1)
+
+    // Get the next tab
+    const nextTab = tabs[nextIndex];
+
+    // Activate the next tab
+    chrome.tabs.update(nextTab.id, { active: true });
+
+}
+
+async function nextTab() {
+
+    // Get all tabs
+    const tabs = await chrome.tabs.query({ currentWindow: true });
+
+    // Find the current active tab
+    const currentTab = tabs.find(tab => tab.active);
+
+    // Find the index of the current tab
+    const currentIndex = tabs.indexOf(currentTab);
+
+    // Calculate the index of the next tab
+    const nextIndex = (currentIndex + 1) % tabs.length;
+
+    // Get the next tab
+    const nextTab = tabs[nextIndex];
+
+    // Activate the next tab
+    chrome.tabs.update(nextTab.id, { active: true });
+
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+    if (message.type === 'prev') {
+        prevTab();
+    }
+
+    if (message.type === 'next') {
+        nextTab();
+    }
+
+});
---
+++ scripts/switcher.js
@@ -0,0 +1,34 @@
+const btnCont = document.createElement('div');
+const prevBtn = document.createElement('button');
+const nextBtn = document.createElement('button');
+
+btnCont.id = 'ktsExtCont';
+prevBtn.id = 'ktsExtPrevBtn';
+nextBtn.id = 'ktsExtNextBtn';
+
+prevBtn.textContent = 'Previous Tab';
+nextBtn.textContent = 'Next Tab';
+
+btnCont.classList.add('kts-ext-cont');
+prevBtn.classList.add('kts-ext-button-default', 'kts-ext-button-prev');
+nextBtn.classList.add('kts-ext-button-default', 'kts-ext-button-next');
+
+const bodyElem = document.querySelector('body');
+
+bodyElem.insertAdjacentElement('afterbegin', btnCont);
+btnCont.appendChild(prevBtn);
+btnCont.appendChild(nextBtn);
+
+function sendPrevMessage() {
+    chrome.runtime.sendMessage({ type: 'prev' });
+}
+
+function sendNextMessage() {
+    chrome.runtime.sendMessage({ type: 'next' });
+}
+
+prevBtn.addEventListener('touchend', sendPrevMessage);
+prevBtn.addEventListener('click', sendPrevMessage);
+
+nextBtn.addEventListener('touchend', sendNextMessage);
+nextBtn.addEventListener('click', sendNextMessage);
